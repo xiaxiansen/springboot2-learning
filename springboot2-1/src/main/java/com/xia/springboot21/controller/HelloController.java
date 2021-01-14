af260295c8bea54c5d792f964f862589a0200884
xialiang <1439741774@qq.com>
2021-01-14 11:13:15 +0800

    1.0

--- springboot2-1/src/main/java/com/xia/springboot21/controller/HelloController.java
+++ springboot2-1/src/main/java/com/xia/springboot21/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.xia.springboot21.controller;
 
+import com.xia.springboot21.entity.Book;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -11,5 +13,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "HelloWorld";
+    }
+    @Autowired
+    Book book;
+    @GetMapping("/book")
+    public String book(){
+        return book.toString();
     }
 }
